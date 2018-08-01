589c900925e364d633348f2acae39ce704ffe1da
Simakeng <Simakeng@outlook.com>
2018-08-01 22:47:26 +0800

    add thread pool

--- BitTorrent-Tracker-Server/BitTorrent-Tracker-Server/BTTServer.cpp
+++ BitTorrent-Tracker-Server/BitTorrent-Tracker-Server/BTTServer.cpp
@@ -6,12 +6,13 @@
 #include <array>
 const std::exception SocketException("socket error");
 
-auto SocketConnectionHandler = [](SOCKET sclient,byte* buffer,int totalLenght)
+auto SocketConnectionHandler = [](SOCKET sClient,byte* buffer,int totalLenght)
 { 
 	delete[] buffer;
+	closesocket(sClient);
 	return;
 };
-auto ServerListenThreadFunc = [](SOCKET& slisten)
+auto ServerListenThreadFunc = [](SOCKET& slisten, std::vector<BTTServer::HttpRequest>& RequestList, std::mutex& ThreadAccessLocker)
 {
 	using BufferArray = std::array<byte, 256>;
 	while (true)
@@ -21,11 +22,12 @@ auto ServerListenThreadFunc = [](SOCKET& slisten)
 			SOCKET sClient;
 			sockaddr_in remoteAddr;
 			int nAddrlen = sizeof(remoteAddr);
-
 			std::vector<BufferArray> RecBuffer;
 			BufferArray Tempbuffer;
 			//等待TCP连接
+			const auto timeout = 3000;
 			sClient = accept(slisten, (SOCKADDR *)&remoteAddr, &nAddrlen);
+			setsockopt(sClient, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(int));
 			if (sClient == INVALID_SOCKET)
 			{
 				continue;
@@ -36,6 +38,7 @@ auto ServerListenThreadFunc = [](SOCKET& slisten)
 			while (RecCount != 0)
 			{
 				RecCount = recv(sClient, (char*)Tempbuffer._Elems, 256, 0);
+				if (RecCount == -1)break;
 				TotalLenth += RecCount;
 				RecBuffer.push_back( Tempbuffer );
 				if (RecBuffer.size() > 1024)
@@ -44,21 +47,41 @@ auto ServerListenThreadFunc = [](SOCKET& slisten)
 					std::vector<BufferArray>().swap(RecBuffer);
 					throw std::exception("Bad Request");
 				}
+				const int tempTimeout = 200;
+				if(RecCount!=256)
+					setsockopt(sClient, SOL_SOCKET, SO_RCVTIMEO, (char *)&tempTimeout, sizeof(int));
 			}
 			auto buffer = new byte[TotalLenth];
 			for (auto i = 0; i < TotalLenth; i++)
 			{
 				buffer[i] = RecBuffer[i / 256][i % 256];
 			}
-			std::thread HandlerTh(SocketConnectionHandler,sClient, buffer, TotalLenth);
+			ThreadAccessLocker.lock();
+			BTTServer::HttpRequest req = { sClient,buffer,TotalLenth };
+			RequestList.push_back(req);
+			ThreadAccessLocker.unlock();
+			//std::thread(SocketConnectionHandler,sClient, buffer, TotalLenth); 这个会因为析构崩溃
 		}
 		catch(std::exception e){}
 	}
 };
-
+auto ServerThreadPoolFunc = [](std::vector<BTTServer::HttpRequest>& RequestList,std::mutex& ThreadAccessLocker)
+{
+	while (true) 
+	{
+		if (RequestList.size() < 8)Sleep(1);
+		if (RequestList.size() == 0)continue;
+		ThreadAccessLocker.lock();
+		if (RequestList.size() == 0)continue;
+		auto req = RequestList[0];
+		RequestList.erase(RequestList.begin());
+		ThreadAccessLocker.unlock();
+		SocketConnectionHandler(req.sClient, req.buffer, req.totalLength);
+	}
+};
 BTTServer::BTTServer(int ListenPort)
 {
-	int hr = 0;
+	auto hr = 0;
 	//初始化
 	hr = WSAStartup(sockVersion, &wsaData);
 	if (hr != 0)throw SocketException;
@@ -66,6 +89,11 @@ BTTServer::BTTServer(int ListenPort)
 	if (slisten == INVALID_SOCKET)throw SocketException;
 
 	this->ListenPort = ListenPort;
+
+	for (auto i = 0; i < threadPool.size(); i++)
+	{
+		threadPool[i] = std::thread(ServerThreadPoolFunc, std::ref(RequestList), std::ref(ThreadAccessLocker));
+	}
 
 	//绑定端口
 	sockaddr_in sin;
@@ -79,7 +107,7 @@ BTTServer::BTTServer(int ListenPort)
 	if (listen(slisten, 5) == SOCKET_ERROR)
 		throw SocketException;
 
-	loopTh = std::thread(ServerListenThreadFunc, std::ref(slisten));
+	loopTh = std::thread(ServerListenThreadFunc, std::ref(slisten), std::ref(RequestList), std::ref(ThreadAccessLocker));
 }
 
 
--- BitTorrent-Tracker-Server/BitTorrent-Tracker-Server/BTTServer.h
+++ BitTorrent-Tracker-Server/BitTorrent-Tracker-Server/BTTServer.h
@@ -1,11 +1,19 @@
 #pragma once
 #include <thread>
-
+#include <array>
+#include <mutex>
+#include <vector>
 class BTTServer
 {
 public:
 	BTTServer(int ListenPort);
 	~BTTServer();
+	struct HttpRequest
+	{
+		SOCKET sClient;
+		byte* buffer;
+		int totalLength;
+	};
 private:
 	const WORD sockVersion = MAKEWORD(2, 2);
 	WSADATA wsaData;
@@ -13,6 +21,8 @@ private:
 	SOCKET slisten;
 
 	std::thread loopTh;
-
+	std::array<std::thread, 8> threadPool;
+	std::vector<HttpRequest> RequestList;
+	std::mutex ThreadAccessLocker;
 };
 
