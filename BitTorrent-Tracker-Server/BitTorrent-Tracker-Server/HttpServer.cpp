8c7fc580d38089d95a8cd5d98a671625550983c1
Simakeng <Simakeng@outlook.com>
2018-08-03 00:55:26 +0800

    HttpRequestDecode

--- BitTorrent-Tracker-Server/BitTorrent-Tracker-Server/HttpServer.cpp
+++ BitTorrent-Tracker-Server/BitTorrent-Tracker-Server/HttpServer.cpp
@@ -2,8 +2,17 @@
 #include "HttpServer.h"
 namespace Utils::HttpServer
 {
-	const std::exception SocketException("socket error");
 
+	auto HttpRequestHandler = [](const std::string& HttpRequestBuffer,const char* const Header)
+	{
+		auto indexStart = HttpRequestBuffer.find(Header) + strlen(Header);
+		auto indexEnd = HttpRequestBuffer.find("\r\n", indexStart);
+		auto indexLength = indexEnd - indexStart;
+		std::string value(HttpRequestBuffer.data() + indexStart, indexLength);
+		return value;
+	};
+	const std::exception SocketException("socket error");
+	const std::exception BadRequestException("bad request");
 	auto ServerListenThreadFunc = [](SOCKET& slisten, std::vector<SOCKET>& RequestList, std::mutex& ThreadAccessLocker)
 	{
 		constexpr int RecvTimeOut = 3000;
@@ -160,5 +169,186 @@ namespace Utils::HttpServer
 	{
 		closesocket(slisten);
 		WSACleanup();
+	}
+	HttpServer::HttpRequest::HttpRequest(const char *HttpRequestBuffer, int64_t bufferLength)
+	{
+		const auto HttpHeadEndLength = strlen("\r\n\r\n");
+		auto reqHeaderEnd = strstr(HttpRequestBuffer, "\r\n\r\n") + HttpHeadEndLength;
+		if (reqHeaderEnd == 0)throw BadRequestException;
+		auto HeaderLength = reqHeaderEnd - HttpRequestBuffer;
+		std::string RequestHeade(HttpRequestBuffer, HeaderLength);
+#pragma region 判断请求类型
+		switch (HttpRequestBuffer[0])
+		{
+		case 'C':
+			this->Method = RequsetMethod::CONNECT;
+			break;
+		case 'D':
+			this->Method = RequsetMethod::DELETE;
+			break;
+		case 'G':
+			this->Method = RequsetMethod::GET;
+			break;
+		case 'H':
+			this->Method = RequsetMethod::HEAD;
+			break;
+		case 'O':
+			this->Method = RequsetMethod::OPTIONS;
+			break;
+		case 'P':
+			if (HttpRequestBuffer[1] == 'O')this->Method = RequsetMethod::POST;
+			else this->Method = RequsetMethod::PUT;
+			break;
+		case 'T':
+			this->Method = RequsetMethod::TRACE;
+		default:
+			throw BadRequestException;
+			break;
+		}
+#pragma endregion
+#pragma region 判断请求链接
+		auto urlStart = RequestHeade.find(" ") + 1;
+		auto urlLength = RequestHeade.find(" ", urlStart) - urlStart;
+		this->RequsetUrl = std::string(RequestHeade.data() + urlStart, urlLength);
+#pragma endregion
+#pragma region Cache_Control处理
+		if (RequestHeade.find("Cache-Control") != -1)
+		{
+		auto Cache_Control_Method = HttpRequestHandler(RequestHeade, "Cache-Control:");
+		this->Cache_Control = CacheControl::NotSupport;
+		if (Cache_Control_Method.find("max-age=") != -1) 
+		{
+			this->Cache_Control = CacheControl::MaxAge;
+			const auto MaxAgeLength = strlen("max-age=");
+			this->Cache_Control_Max_Age = atoi(Cache_Control_Method.data() + MaxAgeLength);
+		}
+		else if (Cache_Control_Method == "public")
+			this->Cache_Control = CacheControl::Public;
+		else if (Cache_Control_Method == "no-cache")
+			this->Cache_Control = CacheControl::NoCache;
+		else if (Cache_Control_Method == "no-store")
+			this->Cache_Control = CacheControl::NoStore;
+		else if (Cache_Control_Method == "must-revalidation")
+			this->Cache_Control = CacheControl::MustRevalidation;
+		else if (Cache_Control_Method == "private")
+			this->Cache_Control = CacheControl::Private;
+		else if (Cache_Control_Method == "proxy-revalidation")
+			this->Cache_Control = CacheControl::ProxyRevalidation;
+		
+		}
+#pragma endregion
+
+
+#pragma region 字符类字段处理
+
+#pragma region Accept字段处理
+		if (RequestHeade.find("Accept:") != -1)
+			this->Accept = HttpRequestHandler(RequestHeade, "Accept:");
+#pragma endregion
+#pragma region Accept-Encoding字段处理
+		if (RequestHeade.find("Accept-Encoding:") != -1)
+			this->Accept_Encoding = HttpRequestHandler(RequestHeade, "Accept-Encoding:");
+#pragma endregion
+#pragma region Accept-Language字段处理
+		if (RequestHeade.find("Accept-Language:") != -1)
+			this->Accept_Language = HttpRequestHandler(RequestHeade, "Accept-Language:");
+#pragma endregion
+#pragma region Accept-Datetime字段处理
+		if (RequestHeade.find("Accept-Datetime") != -1)
+			this->Accept_Datetime = HttpRequestHandler(RequestHeade, "Accept-Datetime:");
+#pragma endregion
+#pragma region Authorization字段处理
+		if (RequestHeade.find("Authorization") != -1)
+			this->Authorization = HttpRequestHandler(RequestHeade, "Authorization:");
+#pragma endregion
+#pragma region Cookie字段处理
+		if (RequestHeade.find("Cookie") != -1)
+			this->Cookie = HttpRequestHandler(RequestHeade, "Cookie:");
+#pragma endregion
+#pragma region Content_MD5字段处理
+		if (RequestHeade.find("Content-MD5") != -1)
+			this->Content_MD5 = HttpRequestHandler(RequestHeade, "Content-MD5:");
+#pragma endregion
+#pragma region Content_Type字段处理
+		if (RequestHeade.find("Content-Type") != -1)
+			this->Content_Type = HttpRequestHandler(RequestHeade, "Content-Type:");
+#pragma endregion
+#pragma region Expect字段处理
+		if (RequestHeade.find("Expect") != -1)
+			this->Expect = HttpRequestHandler(RequestHeade, "Expect:");
+#pragma endregion
+#pragma region From字段处理
+		if (RequestHeade.find("From") != -1)
+			this->From = HttpRequestHandler(RequestHeade, "From:");
+#pragma endregion
+#pragma region Host字段处理
+		if (RequestHeade.find("Host") != -1)
+			this->Host = HttpRequestHandler(RequestHeade, "Host:");
+#pragma endregion
+#pragma region If_Match字段处理
+		if (RequestHeade.find("If-Match") != -1)
+			this->If_Match = HttpRequestHandler(RequestHeade, "If-Match:");
+#pragma endregion
+#pragma region If_Modified_Since字段处理
+		if (RequestHeade.find("If-Modified-Since") != -1)
+			this->If_Modified_Since = HttpRequestHandler(RequestHeade, "If-Modified-Since:");
+#pragma endregion
+#pragma region If_None_Match字段处理
+		if (RequestHeade.find("If-None-Match") != -1)
+			this->If_None_Match = HttpRequestHandler(RequestHeade, "If-None-Match:");
+#pragma endregion
+#pragma region If_Range字段处理
+		if (RequestHeade.find("If-Range") != -1)
+			this->If_Range = HttpRequestHandler(RequestHeade, "If-Range:");
+#pragma endregion
+#pragma region If_Unmodified_Since字段处理
+		if (RequestHeade.find("If-Unmodified-Since") != -1)
+			this->If_Unmodified_Since = HttpRequestHandler(RequestHeade, "If-Unmodified-Since:");
+#pragma endregion
+#pragma region Origin字段处理
+		if (RequestHeade.find("Origin") != -1)
+			this->Origin = HttpRequestHandler(RequestHeade, "Origin:");
+#pragma endregion
+#pragma region Pragma字段处理
+		if (RequestHeade.find("Pragma") != -1)
+			this->Pragma = HttpRequestHandler(RequestHeade, "Pragma:");
+#pragma endregion
+#pragma region Proxy_Authorization字段处理
+		if (RequestHeade.find("Proxy-Authorization") != -1)
+			this->Proxy_Authorization = HttpRequestHandler(RequestHeade, "Proxy-Authorization:");
+#pragma endregion
+#pragma region Range字段处理
+		if (RequestHeade.find("Range") != -1)
+			this->Range = HttpRequestHandler(RequestHeade, "Range:");
+#pragma endregion
+#pragma region Referer字段处理
+		if (RequestHeade.find("Referer") != -1)
+			this->Referer = HttpRequestHandler(RequestHeade, "Referer:");
+#pragma endregion
+#pragma region TE字段处理
+		if (RequestHeade.find("TE") != -1)
+			this->TE = HttpRequestHandler(RequestHeade, "TE:");
+#pragma endregion
+#pragma region User_Agent字段处理
+		if (RequestHeade.find("User-Agent") != -1)
+			this->User_Agent = HttpRequestHandler(RequestHeade, "User-Agent:");
+#pragma endregion
+#pragma region Upgrade字段处理
+		if (RequestHeade.find("Upgrade") != -1)
+			this->Upgrade = HttpRequestHandler(RequestHeade, "Upgrade:");
+#pragma endregion
+#pragma region Via字段处理
+		if (RequestHeade.find("Via") != -1)
+			this->Via = HttpRequestHandler(RequestHeade, "Via:");
+#pragma endregion
+#pragma region Warning字段处理
+		if (RequestHeade.find("Warning") != -1)
+			this->Warning = HttpRequestHandler(RequestHeade, "Warning:");
+#pragma endregion
+#pragma endregion
+
+	}
+	HttpServer::HttpRequest::~HttpRequest()
+	{
 	}
 }
--- BitTorrent-Tracker-Server/BitTorrent-Tracker-Server/HttpServer.h
+++ BitTorrent-Tracker-Server/BitTorrent-Tracker-Server/HttpServer.h
@@ -3,7 +3,7 @@
 #include <array>
 #include <mutex>
 #include <vector>
-
+#include <string>
 #include "Event.h"
 namespace Utils::HttpServer
 {
@@ -37,6 +37,128 @@ namespace Utils::HttpServer
 	public:
 		using EventHttpSocketRequestReceved = Event::Event<HttpSocketRequest>;
 		EventHttpSocketRequestReceved HttpSocketRequestRecevedEvent;
+	public:
+		class HttpRequest
+		{
+		public:
+#pragma region 构造,析构函数
+			HttpRequest() = delete;
+			HttpRequest(SOCKET sClient);
+			HttpRequest(const char* HttpRequestBuffer,int64_t bufferLength);
+			~HttpRequest();
+#pragma endregion
+#undef DELETE
+		public:
+#pragma region 请求上报的字段数据定义
+			//请求类型
+			enum class RequsetMethod
+			{
+				//请求指定的页面信息，并返回实体主体。
+				GET,
+				//类似于GET请求，只不过返回的响应中没有具体的内容，用于获取报头
+				HEAD,
+				//向指定资源提交数据进行处理请求（例如提交表单或者上传文件）。数据被包含在请求体中。POST请求可能会导致新的资源的建立和/或已有资源的修改。
+				POST,
+				//	从客户端向服务器传送的数据取代指定的文档的内容。
+				PUT,
+				//请求服务器删除指定的页面。
+				DELETE,
+				//HTTP/1.1协议中预留给能够将连接改为管道方式的代理服务器。
+				CONNECT,
+				//允许客户端查看服务器的性能。
+				OPTIONS,
+				//回显服务器收到的请求，主要用于测试或诊断。
+				TRACE
+			};
+			//请求类型
+			using string = std::string; //Magic ?
+			RequsetMethod Method;
+			string RequsetUrl = "";
+			string Accept = "";
+			string Accept_Charset = "";
+			string Accept_Encoding = "";
+			string Accept_Language = "";
+			string Accept_Datetime = "";
+			string Authorization = "";
+			enum class CacheControl
+			{
+				//所有内容都将被缓存(客户端和代理服务器都可缓存)
+				Public,
+				//内容只缓存到私有缓存中(仅客户端可以缓存，代理服务器不可缓存)
+				Private,
+				//必须先与服务器确认返回的响应是否被更改，然后才能使用该响应来满足后续对同一个网址的请求。因此，如果存在合适的验证令牌 (ETag)，no-cache 会发起往返通信来验证缓存的响应，如果资源未被更改，可以避免下载。
+				NoCache,
+				//所有内容都不会被缓存到缓存或 Internet 临时文件中
+				NoStore,
+				//如果缓存的内容失效，请求必须发送到服务器以进行重新验证
+				MustRevalidation,
+				//如果缓存的内容失效，请求必须发送到代理以进行重新验证
+				ProxyRevalidation,
+				//缓存的内容将在 Cache_Control_Max_Age 秒后失效, 这个选项只在HTTP 1.1可用, 并如果和Last-Modified一起使用时, 优先级较高
+				MaxAge,
+				//请求的客户端没有上报此字段
+				None,
+				//请求的客户端上报的字段此服务器不支持
+				NotSupport
+			};
+			CacheControl Cache_Control = CacheControl::None;
+			int64_t Cache_Control_Max_Age = 0;
+			enum class ConnectionType 
+			{
+				//长连接
+				KeepAlive,
+				//短链接
+				Close,
+				//请求的客户端没有上报此字段
+				None
+			};
+			ConnectionType Connection = ConnectionType::None;
+			string Cookie = "";
+			int64_t Content_Length = 0;
+			string Content_MD5 = "";
+			string Content_Type = "";
+			struct HttpDateTime 
+			{
+				int Year;
+				int Month;
+				int Day;
+				int Hour;
+				int Minute;
+				int Second;
+				enum class week
+				{
+					Mon = 1,
+					Tues = 2,
+					Wed = 3,
+					Thur = 4,
+					Fri = 5,
+					Sat = 6,
+					Sun = 7
+				};
+				week WeekDat;
+			};
+			HttpDateTime Date = { 0 };
+			string Expect = "";
+			string From = "";
+			string Host = "";
+			string If_Match = "";
+			string If_Modified_Since = "";
+			string If_None_Match = "";
+			string If_Range = "";
+			string If_Unmodified_Since = "";
+			int Max_Forwards = 0;
+			string Origin = "";
+			string Pragma = "";
+			string Proxy_Authorization = "";
+			string Range = "";
+			string Referer = "";
+			string TE = "";
+			string User_Agent = "";
+			string Upgrade = "";
+			string Via = "";
+			string Warning = "";
+#pragma endregion
+		};
 	};
 	using HttpSocketRequest = HttpServer::HttpSocketRequest;
 
